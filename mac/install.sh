f49af3d428141feecd6b14dd65d68892b0fca6ab
Rodolfo Navalon <rodolfonavalon@Rodolfos-Mac-mini.local>
2020-01-08 14:27:17 -0500

    Added more alias for merge and diff tool and added p4merge as tool in the alias.

--- mac/install.sh
+++ mac/install.sh
@@ -7,15 +7,17 @@ install-base() {
 	curl -L -s -H 'Cache-Control: no-cache' -o installer-rodolfo-util.sh https://raw.githubusercontent.com/poldz123/EnvironmentInstaller/master/util.sh && . ./installer-rodolfo-util.sh
 	rm -f installer-rodolfo-util.sh
 	# Install brew: https://brew.sh/
-	util-print-header "Installing BREW"
-	echo | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	if ! which brew > /dev/null; then
+		util-print-header "Installing BREW"
+		echo | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
 }
 
 install-terminal() {
 	util-print-header "Installing ZSH-TERMINAL"
 	brew cask install iterm2
 	brew install zsh
-	echo | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	echo | sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" | true
 	# Create the alias for git
 	util-append-unique-text-to-file 'alias gpl="git pull"' "$HOME/.zshrc"
 	util-append-unique-text-to-file 'alias gph="git push"' "$HOME/.zshrc"
@@ -23,6 +25,9 @@ install-terminal() {
 	util-append-unique-text-to-file 'alias gs="git status"' "$HOME/.zshrc"
 	util-append-unique-text-to-file 'alias gca="git commit --amend"' "$HOME/.zshrc"
 	util-append-unique-text-to-file 'alias gd="git diff"' "$HOME/.zshrc"
+	util-append-unique-text-to-file 'alias gdt="git difftool -y --tool=p4merge"' "$HOME/.zshrc"
+	util-append-unique-text-to-file 'alias gm="git merge"' "$HOME/.zshrc"
+	util-append-unique-text-to-file 'alias gmt="git mergetool -y --tool=p4merge"' "$HOME/.zshrc"
 	util-append-unique-text-to-file 'alias gaa="git add ."' "$HOME/.zshrc"
 	util-append-unique-text-to-file 'alias gl="git log --pretty=oneline"' "$HOME/.zshrc"
 }
@@ -32,6 +37,7 @@ install-applications() {
 	brew cask install spotify
 	brew cask install google-chrome
 	brew cask install android-studio
+	brew cask install p4v
 }
 
 install-base
