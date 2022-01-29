454814cbbb100cfa544a07584815bbc64ae10310
rodolfo navalon <navalonrodolfo@gmail.com>
2022-01-29 01:06:57 -0500

    Update install.sh

--- mac/install.sh
+++ mac/install.sh
@@ -15,13 +15,14 @@ install-base() {
 
 install-java() {
 	util-print-header "Installing JAVA-JDK-8"
-	brew cask install adoptopenjdk/openjdk/adoptopenjdk8
+	brew install --cask AdoptOpenJDK/openjdk/adoptopenjdk8
+	brew install --cask AdoptOpenJDK/openjdk/adoptopenjdk11
 }
 
 install-terminal() {
 	util-print-header "Installing ZSH-TERMINAL"
 	touch ~/.zshrc # Create the file so that zsh wont break during installation
-	brew cask install iterm2
+	brew install --cask iterm2
 	brew install zsh
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" | true
 	# Create the alias for git
@@ -49,10 +50,13 @@ install-terminal() {
 	util-append-unique-text-to-file 'export PATH=$PATH:$ANDROID_HOME/tools/bin' "$HOME/.zshrc"
 	util-append-unique-text-to-file 'export PATH=$PATH:$ANDROID_HOME/platform-tools' "$HOME/.zshrc"
 	util-append-unique-text-to-file 'export PATH=$PATH:$ANDROID_AVD_HOME' "$HOME/.zshrc"
+	util-append-unique-text-to-file 'export PATH=$HOME/.jenv/bin:$PATH' "$HOME/.zshrc"
+	util-append-unique-text-to-file 'eval "$(jenv init -)"' "$HOME/.zshrc"
 }
 
 install-kubernetes() {
 	util-print-header "Installing KUBERNETES"
+	brew install --cask docker
 	brew install kubectl
 	brew install kubectx
 	# Create an empty config, this wont be empty if it was already created before
@@ -80,18 +84,17 @@ install-commands() {
 
 install-applications() {
 	util-print-header "Installing BREW-APPLICATIONS"
-	brew cask install spotify
-	brew cask install google-chrome
-	brew cask install android-studio
-	brew cask install sublime-text
-	brew cask install p4v
-	brew cask install virtualbox
-	brew cask install db-browser-for-sqlite
-	brew cask install android-file-transfer
-	brew cask install intellij-idea-ce
-	brew cask install docker
-	brew cask install postman
-	brew cask install visual-studio-code
+	brew install --cask spotify
+	brew install --cask google-chrome
+	brew install --cask android-studio
+	brew install --cask sublime-text
+	brew install --cask p4v
+	brew install --cask virtualbox
+	brew install --cask db-browser-for-sqlite
+	brew install --cask android-file-transfer
+	brew install --cask intellij-idea-ce
+	brew install --cask postman
+	brew install --cask visual-studio-code
 }
 
 install-addon() {
